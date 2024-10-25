bcde4914405ea974f540a18ba63e434276fac01c
Victoria Rose <rad.vicisrad@gmail.com>
2024-10-25 15:28:30 -0400

    selecting starting and target currencies set up

--- Numbers/Unit_Converter/src/main/java/com/CurrencyConvert.java
+++ Numbers/Unit_Converter/src/main/java/com/CurrencyConvert.java
@@ -27,28 +27,97 @@ public class CurrencyConvert {
         final String NGN_FORMAT = "Nigerian Naira";
         final String USD_FORMAT = "United States Dollar";
 
-        System.out.println("Please select the starting currency;");
-        System.out.println("1. " + ARS_FORMAT);
-        System.out.println("2. " + CAD_FORMAT);
-        System.out.println("3. " + EUR_FORMAT);
-        System.out.println("4. " + INR_FORMAT);
-        System.out.println("5. " + JPY_FORMAT);
-        System.out.println("6. " + KWD_FORMAT);
-        System.out.println("7. " + NGN_FORMAT);
-        System.out.println("8. " + USD_FORMAT);
+        String startingCurrencyName = "";
+
+        while (startingCurrencyName.equals("")) {
+            System.out.println("Please select a starting currency;");
+            System.out.println("1. " + ARS_FORMAT);
+            System.out.println("2. " + CAD_FORMAT);
+            System.out.println("3. " + EUR_FORMAT);
+            System.out.println("4. " + INR_FORMAT);
+            System.out.println("5. " + JPY_FORMAT);
+            System.out.println("6. " + KWD_FORMAT);
+            System.out.println("7. " + NGN_FORMAT);
+            System.out.println("8. " + USD_FORMAT);
+
+            int startingCurrencyID = Integer.parseInt(keyboard.nextLine());
 
 
-        String startingCurrencyID = keyboard.nextLine();
-        System.out.println("You have selected " + startingCurrencyID + " as your starting currency." + "\n" +
-                "Please select a currency to convert to;");
-        System.out.println("1. " + ARS_FORMAT);
-        System.out.println("2. " + CAD_FORMAT);
-        System.out.println("3. " + EUR_FORMAT);
-        System.out.println("4. " + INR_FORMAT);
-        System.out.println("5. " + JPY_FORMAT);
-        System.out.println("6. " + KWD_FORMAT);
-        System.out.println("7. " + NGN_FORMAT);
-        System.out.println("8. " + USD_FORMAT);
+            if (startingCurrencyID >= 1 && startingCurrencyID <=8) {
+                if (startingCurrencyID == 1) {
+                    startingCurrencyName = ARS_FORMAT;
+                } else if (startingCurrencyID == 2) {
+                    startingCurrencyName = CAD_FORMAT;
+                } else if (startingCurrencyID == 3) {
+                    startingCurrencyName = EUR_FORMAT;
+                } else if (startingCurrencyID == 4) {
+                    startingCurrencyName = INR_FORMAT;
+                } else if (startingCurrencyID == 5) {
+                    startingCurrencyName = JPY_FORMAT;
+                } else if (startingCurrencyID == 6) {
+                    startingCurrencyName = KWD_FORMAT;
+                } else if (startingCurrencyID == 7) {
+                    startingCurrencyName = NGN_FORMAT;
+                } else {
+                    startingCurrencyName = USD_FORMAT;
+                }
+            } else {
+                System.out.println("Please type a number between 1 and 8 to select a starting currency.");
+            }
+        }
+
+
+
+
+        System.out.println("You have selected " + startingCurrencyName + " as your starting currency." + "\n");
+
+        String targetCurrencyName = "";
+
+        while (targetCurrencyName.equals("")) {
+            System.out.println("Please select a currency to convert to;");
+            System.out.println("1. " + ARS_FORMAT);
+            System.out.println("2. " + CAD_FORMAT);
+            System.out.println("3. " + EUR_FORMAT);
+            System.out.println("4. " + INR_FORMAT);
+            System.out.println("5. " + JPY_FORMAT);
+            System.out.println("6. " + KWD_FORMAT);
+            System.out.println("7. " + NGN_FORMAT);
+            System.out.println("8. " + USD_FORMAT);
+
+            int targetCurrencyID = Integer.parseInt(keyboard.nextLine());
+            if (targetCurrencyID >= 1 && targetCurrencyID <=8) {
+                if (targetCurrencyID == 1) {
+                    targetCurrencyName = ARS_FORMAT;
+                } else if (targetCurrencyID == 2) {
+                    targetCurrencyName = CAD_FORMAT;
+                } else if (targetCurrencyID == 3) {
+                    targetCurrencyName = EUR_FORMAT;
+                } else if (targetCurrencyID == 4) {
+                    targetCurrencyName = INR_FORMAT;
+                } else if (targetCurrencyID == 5) {
+                    targetCurrencyName = JPY_FORMAT;
+                } else if (targetCurrencyID == 6) {
+                    targetCurrencyName = KWD_FORMAT;
+                } else if (targetCurrencyID == 7) {
+                    targetCurrencyName = NGN_FORMAT;
+                } else {
+                    targetCurrencyName = USD_FORMAT;
+                }
+            } else {
+                System.out.println("Please type a number between 1 and 8 to select a target currency." + "\n");
+            }
+        }
+
+        System.out.println("You have selected " + targetCurrencyName + " as your target currency." + "\n");
+
+        System.out.println("Please enter the amount of money in " + startingCurrencyName + " format to convert to " +
+                targetCurrencyName + " format.");
+
+        
+
+
+
+
 
 
 
